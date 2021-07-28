6c682ae535972ad10dd7772620f5d68cc6535ea5
Luis Hernández <58614359+CapitanMazapan@users.noreply.github.com>
2021-07-28 00:19:35 -0500

    Implement methods to create the session and connect it.

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const ora = require('ora');
+const chalk = require('chalk');
 
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
-});
+const SESSION_FILE_PATH = './session.json';
+let client;
+let sessionData;
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+const withSession = () => {
+    const spinner = ora(`Cargando ${chalk.yellow('Validando sesión con Whatsapp...')}`);
+    sessionData = require(SESSION_FILE_PATH);
+    spinner.start();
 
-client.initialize();
+    client = new Client({
+        session:sessionData
+    })
+
+    client.on('ready', () => {
+        console.log('¡Conectado!');
+        spinner.stop();
+    });
+
+    client.on('auth_failure', () => {
+        spinner.stop();
+        console.log('Error de autenticación, borra el archivo session.json y vuelve a generar el código QR.')
+    })
+
+    client.initialize();
+}
+
+const withOutSession = () => {
+    console.log('No se ha iniciado una sesión.');
+    client = new Client();
+
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, {small: true})
+    });
+
+    client.on('authenticated', (session) => {
+        sessionData = session;
+        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+
+    client.initialize();
+}
+
+(fs.existsSync(SESSION_FILE_PATH)) ? withSession() : withOutSession();
